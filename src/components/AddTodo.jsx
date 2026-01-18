79ff7fa4f43f8c101078178132dc811a28cc645c
Jeevansh Grover <groverjeevansh0243@gmail.com>
2026-01-19 00:40:54 +0530

    create addTodo and todo components

---
+++ src/components/AddTodo.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { addTodo } from '../features/todoSlice.js' 
+
+function AddTodo() {
+    const {input, setInput} = useState('')
+    const dispatch = useDispatch()
+    const addTodoHandler = (e) => {
+        e.preventDefault()
+        dispatch(addTodo(input))
+        setInput('')
+    }
+
+  return (
+    <form onSubmit={addTodoHandler}>
+        <input
+            type='text'
+            placeholder='enter a todo'
+            value={input}
+            onChange = {(e) => setInput(e.target.value)}
+        />
+
+        <button
+            type = 'submit'
+            // className=''
+        >
+            Add todo
+        </button>
+    </form>
+  )
+}
+
+export default AddTodo
